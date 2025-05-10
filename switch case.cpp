f6424c238ab07817a8f833749ac53641728f13fc
warrior8008 <ghosharup218@gmail.com>
2025-05-10 14:09:10 +0530

    1st commit

---
+++ switch case.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+    int main(){
+
+        int num;
+
+            printf("Enter the value of num = ");
+            scanf("%d", &d);
+
+            switch (a){
+                case 1:
+                    printf()
+
+
+
+
+            }
+
+
+     return 0;
+    }
