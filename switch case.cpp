e0b4a31fdb5c6aa4cac454eaac60317b3f9bfea9
warrior8008 <ghosharup218@gmail.com>
2025-05-11 12:03:20 +0530

    Auto update: Sun May 11 12:03:20 PM IST 2025

--- switch case.cpp
+++ switch case.cpp
@@ -14,11 +14,23 @@
                 case 2:
                     printf("You enter 2");
                     break;
+                case 3:
+                    printf("You enter 3");
+                    break;
+                case 4:
+                    printf("You enter 4");
+                    break;
+                case 5:
+                    printf("You enter 5");
+                    break;
+                case 6:
+                    printf("You enter 6");
+                    break;
+                case 7:
+                    printf("You enter 7");
+                    break;                
                 default:
                     printf("You enter unwanted value.");
-
-
-
             }
 
 
