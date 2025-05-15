b347bdd655fa7ffeda14cd17d0bd03322d2addf2
warrior8008 <ghosharup218@gmail.com>
2025-05-15 19:52:45 +0530

    Auto update: Thu May 15 07:52:45 PM IST 2025

--- prac.c
+++ prac.c
@@ -4,6 +4,13 @@ int main(){
     
         int i = 5;
 
+            /* i++ = Isme I ka matlab hai ki i ka jo value hai use print kar do, aur phir process +1 kar do isme print nahi karn hai abhi
+                  Isiliye jab hum i ko baad me print kar rh hai to hame processed value mil rhi hai */
+
+
+            /* Aur isi ka reverse hota hai i-- me ya --i me */
+
+
             printf("The value of i is %d\n", i++);
             printf("The value of i is %d\n", i);
 
