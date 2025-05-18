8615cdd8bd22bdd6e6351e81bd7ccd46889b987e
warrior8008 <ghosharup218@gmail.com>
2025-05-18 17:27:03 +0530

    Auto update: Sun May 18 05:27:03 PM IST 2025

---
+++ Do-while.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+    int main(){
+
+        
+
+
+
+
+
+
+1
+        return 0;
+    }
--- loop.c
+++ loop.c
@@ -6,13 +6,17 @@ int main(){
     
         int i = 0;
 
-        while(i<20){
+        while(i<=20){
             
-            if(i<=20){
+            if(i>=10){
                 
-                printf("The value of a is %d\n", i);
+                printf("The value of i is %d\n", i); 
+              
+             }
+
                 i++;
-            }
+                
+
         }
     return 0;
 }
