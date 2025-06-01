2ff4aa8c001152e87a0d9ee9fdb8434391c721d5
warrior8008 <ghosharup218@gmail.com>
2025-06-01 13:03:58 +0530

    Auto update: Sun Jun  1 01:03:58 PM IST 2025

--- practice/practice.c
+++ practice/practice.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
 // Function ka declaration (prototype)
-int sum(int x, int y);
+int sum(int , int);
+
+// Ye function hai – do numbers lega aur sum karke return karega
+int sum(int x, int y) {
+    return x + y ;
+}
+
 
 // Main function – yahin se program start hota hai
 int main() {
-    int a, b;
+    int a, b ;
 
-    printf("Enter two numbers: , ");
-    scanf("%d  %d", &a, &b);
+    printf("Enter 1st number:");
+    scanf("%d", &a);
+
+    printf("Enter 2nd number:");
+    scanf("%d", &b);
+
+    
 
     int result = sum(a, b);  // Function ko call kiya
 
     printf("The sum is: %d\n", result);
 
     return 0;
-}
-
-// Ye function hai – do numbers lega aur sum karke return karega
-int sum(int x, int y) {
-    return x + y;
 }
---
+++ practice/practice_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("1\n");
+    printf("2 3\n");
+    printf("4 5 6\n");
+    printf("7 8 9 10\n");
+
+
+    return 0;
+}
